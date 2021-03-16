a4d40a5cf047444a85fedc388391a45e02dae8a4
onurinal <onur.inall01@gmail.com>
2021-03-16 04:08:30 +0300

    Day 1.

---
+++ Make Nullable Types/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Make_Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? areYouMaster = null;
+
+            if(areYouMaster == true)
+            {
+                Console.WriteLine("You are master.");
+            }
+            else if(areYouMaster == false)
+            {
+                Console.WriteLine("You are not master.");
+            }
+            else
+            {
+                Console.WriteLine("You did not answer the question.");
+            }
+
+            int? ticketOnSale = null;
+            int availableTickets;
+
+            if(ticketOnSale == null)
+            {
+                availableTickets = 0;
+            }
+            else
+            {
+                availableTickets = (int)ticketOnSale;
+            }
+            Console.WriteLine("Available tickets " + availableTickets);
+        }
+    }
+}
---
+++ Read And Write/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Read_And_Write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name and surname");
+            string user = Console.ReadLine();
+            Console.WriteLine("Enter your number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+          
+            Console.WriteLine("Your name and surname: " + user + "\nYour number: " + userNumber );
+        }
+    }
+}
